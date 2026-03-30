45afccdeb729213058c0df9387b162286c054e52
lunarMaxar <racelar.hoo@gmail.com>
2026-03-30 17:25:19 -0400

    update

--- js/app.js
+++ js/app.js
@@ -3,8 +3,10 @@ import { PCDLoader } from 'three/addons/loaders/PCDLoader.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 const container = document.getElementById('canvas-container');
+const mediaContainer = document.getElementById('media-container')
 const scene = new THREE.Scene();
 
+const rect = mediaContainer.getBoundingClientRect()
 const aspect = window.innerWidth / (window.innerHeight * 0.6);
 const frustumSize = 0.16;
 const camera = new THREE.OrthographicCamera(
@@ -70,6 +72,7 @@ toggleBtn.addEventListener('click', () => {
 });
 
 window.addEventListener('resize', () => {
+    const newRect = mediaContainer.getBoundingClientRect();
     const newAspect = window.innerWidth / (window.innerHeight * 0.6);
     camera.left = (frustumSize * newAspect) / -2;
     camera.right = (frustumSize * newAspect) / 2;
